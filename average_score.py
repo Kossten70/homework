b066da36d1c90e85e8f7ea41b49c4ca71f2a805d
Konstantin <ref6512722@gmail.com>
2024-09-23 15:19:28 +0300

    средняя оценка

--- average_score.py
+++ average_score.py
@@ -1,4 +1,5 @@
 grades = [[5 ,3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Stieve', 'Khendrik', 'Aaron'}
 students = sorted(students)
-print(students)
+print(grades)
+print(students[0],((sum(grades[0]))/ len(grades[0])))
